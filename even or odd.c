8c9cf0a7e4c988e1b812540679fda31c0ebbb709
Md Tanvir Ahamed Shanto <112241372+mdtanvirahamedshanto@users.noreply.github.com>
2022-12-21 20:37:46 +0600

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,18 @@
+//This Code Write By Md Tanvir Ahamed Shanto
+
+
+#include <stdio.h>
+int main()
+{
+  int n;
+
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+
+  if (n%2 == 0)
+    printf("Even\n");
+  else
+    printf("Odd\n");
+
+  return 0;
+}
